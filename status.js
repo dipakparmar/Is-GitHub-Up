f92d0b9068bbfffaa2db3cdd7f0fd59809862675
Dipak Parmar <hi@dipak.tech>
2021-04-24 19:51:36 -0700

    Update api to v2

--- status.js
+++ status.js
@@ -1,29 +1,24 @@
 var request = require('request');
 
-var url = 'https://status.github.com/api/status.json';
-
+var url = 'https://status.github.com/api/v2/status.json';
 
 request({
-    url: url,
-    json: true
+	url: url,
+	json: true
 }, function (error, response, body) {
 
-    if (!error && response.statusCode === 200) {
-        
-			if(body.status=='good'){
+	if (!error && response.statusCode === 200) {
+
+		if (body.status.indicator == 'none') {
 			console.log("✅ Everything operating normally.");
-			console.log("Last Updated: "+body.last_updated);
-			}
-			
-			else if(body.status=='minor'){
-				console.log("⚠️ Some minor issues, effecting the server.");
-				console.log("Last Updated: "+body.last_updated);
-			}
-			else{
-			console.log("❌ Server is Down.");
-			console.log("Last Updated: "+body.last_updated);
-			}
-    }
+			console.log("Last Updated: " + body.page.updated_at);
+		}
+
+		else {
+			console.log("⚙️ Something is wrong!");
+			console.log("Last Updated: " + body.page.updated_at);
+		}
+	}
 });
 
 
